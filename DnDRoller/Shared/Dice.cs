7ce543fca64837e2a03a856fca4bf84654921d33
Mikko <mikko.roysko@gmail.com>
2022-03-05 14:44:45 +0200

    Tabs to spaces

--- DnDRoller/Shared/Dice.cs
+++ DnDRoller/Shared/Dice.cs
@@ -1,13 +1,13 @@
 ﻿namespace DnDRoller.Shared
 {
-	public enum Dice
-	{
-		d4 = 4,
-		d6 = 6,
-		d8 = 8,
-		d10 = 10,
-		d12 = 12,
-		d20 = 20,
-		d100 = 100
-	}
+    public enum Dice
+    {
+        d4 = 4,
+        d6 = 6,
+        d8 = 8,
+        d10 = 10,
+        d12 = 12,
+        d20 = 20,
+        d100 = 100
+    }
 }
--- DnDRoller/Shared/Group.cs
+++ DnDRoller/Shared/Group.cs
@@ -2,27 +2,27 @@
 {
     public class Group
     {
-		private int count = 1;
-		public int Count
-		{
-			get => count;
-			set => count = Math.Max(1, value);
-		}
+        private int count = 1;
+        public int Count
+        {
+            get => count;
+            set => count = Math.Max(1, value);
+        }
 
-		private int nd = 1;
-		public int Nd
-		{
-			get => nd;
-			set => nd = Math.Max(1, value);
-		}
+        private int nd = 1;
+        public int Nd
+        {
+            get => nd;
+            set => nd = Math.Max(1, value);
+        }
 
-		public int AC = 10;
+        public int AC = 10;
 
-		public int HitPlus = 0;
-		public int DamagePlus = 0;
+        public int HitPlus = 0;
+        public int DamagePlus = 0;
 
 
-		public Dice D = Dice.d8;
-		public List<ThrowResult> Results = new List<ThrowResult>();
-	}
+        public Dice D = Dice.d8;
+        public List<ThrowResult> Results = new List<ThrowResult>();
+    }
 }
